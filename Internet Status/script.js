0f3500d46ec384bbd5c7c6429e9ffd8e8078532b
João Marcos <joaomarcos.vidcar@gmail.com>
2023-03-15 08:02:24 -0300

    Internet Status Done

--- Internet Status/script.js
+++ Internet Status/script.js
@@ -6,7 +6,7 @@ const infoList = [
   "You're ONLINE!!! Connection looks good."
 ]
 
-window.addEventListener('load', e => {
+/*window.addEventListener('load', e => {
   if (navigator.onLine) {
     imageStatus.src = imageList[1]
     infoStatus.innerHTML = infoList[1]
@@ -15,3 +15,27 @@ window.addEventListener('load', e => {
     infoStatus.innerHTML = infoList[0]
   }
 })
+*/
+
+async function connectionStatus() {
+  try {
+    const fetchResult = await fetch(
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Arizona_%28BB39%29_Port_Bow%2C_Underway_-_NARA_-_5900075_-_1930.jpg/200px-Arizona_%28BB39%29_Port_Bow%2C_Underway_-_NARA_-_5900075_-_1930.jpg?time=' +
+        new Date().getTime()
+    )
+    console.log(fetchResult)
+    imageStatus.src = imageList[1]
+    infoStatus.innerHTML = infoList[1]
+    return fetchResult.status >= 200 && fetchResult.status < 300
+  } catch (error) {
+    console.log(error)
+    imageStatus.src = imageList[0]
+    infoStatus.innerHTML = infoList[0]
+  }
+}
+
+window.addEventListener('load', connectionStatus)
+
+setInterval(async () => {
+  await connectionStatus()
+}, 5000)
