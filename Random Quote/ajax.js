6055b4c8b5fb8a03ff5f28b80adeaea98c377de6
João Marcos <joaomarcos.vidcar@gmail.com>
2023-03-24 09:32:01 -0300

    Adding Number of Quotes Feature

--- Random Quote/ajax.js
+++ Random Quote/ajax.js
@@ -1,22 +1,39 @@
 const genButton = document.querySelector('#genQuote')
-const quote = document.querySelector('#quote')
-const author = document.querySelector('#author')
+// const quote = document.querySelector('.quote')
+// const author = document.querySelector('.author')
+const quoteNum = document.querySelector('#quoteNum')
+const quoteLists = document.querySelector('#quoteLists')
+const quoteList = document.createElement('div')
+quoteList.className = 'quoteList'
 
 genButton.addEventListener('click', genQuote)
 
 function genQuote() {
-  var random = 10000
+  quoteLists.innerHTML = ''
+  quoteList.innerHTML = ''
+  const times = quoteNum.value
   xhttp = new XMLHttpRequest()
   xhttp.open('GET', 'https://type.fit/api/quotes', true)
   xhttp.send()
   xhttp.onload = function () {
     const quotes = JSON.parse(this.responseText)
-    while (random > quotes.length) {
-      random = Math.floor(Math.random() * 10000) - 1
+
+    for (let i = 0; i < times; i++) {
+      var random = 10000
+      while (random > quotes.length) {
+        random = Math.floor(Math.random() * 10000) - 1
+      }
+
+      const fullQuote = document.createElement('div')
+      fullQuote.innerHTML = `<p class="quote">${quotes[random].text}</p><p class="author">${quotes[random].author}</p>`
+      fullQuote.className = 'fullQuote'
+      quoteList.appendChild(fullQuote)
+
+      console.log(random)
+      console.log(quotes.length)
+      //quote.innerHTML = quotes[random].text
+      //author.innerHTML = quotes[random].author
     }
-    console.log(random)
-    console.log(quotes.length)
-    quote.innerHTML = quotes[random].text
-    author.innerHTML = quotes[random].author
+    quoteLists.appendChild(quoteList)
   }
 }
--- Random Quote/fetch.js
+++ Random Quote/fetch.js
@@ -1,24 +1,41 @@
 const genButton = document.querySelector('#genQuote')
-const quote = document.querySelector('#quote')
-const author = document.querySelector('#author')
+// const quote = document.querySelector('#quote')
+// const author = document.querySelector('#author')
+const quoteNum = document.querySelector('#quoteNum')
+const quoteLists = document.querySelector('#quoteLists')
+const quoteList = document.createElement('div')
+quoteList.className = 'quoteList'
 
 genButton.addEventListener('click', genQuote)
 
 function genQuote() {
+  const times = quoteNum.value
+  quoteLists.innerHTML = ''
+  quoteList.innerHTML = ''
+
   fetch('https://type.fit/api/quotes')
     .then(function (response) {
       return response.json()
     })
     .then(function (data) {
       quotes = data
-      let random = 10000
-      while (random > quotes.length) {
-        random = Math.floor(Math.random() * 10000) - 1
+      for (let i = 0; i < times; i++) {
+        let random = 10000
+        while (random > quotes.length) {
+          random = Math.floor(Math.random() * 10000) - 1
+        }
+
+        const fullQuote = document.createElement('div')
+        fullQuote.className = 'fullQuote'
+        fullQuote.innerHTML = `<p class='quote'>${quotes[random].text}</p><p class='author'>${quotes[random].author}</p>`
+        quoteList.appendChild(fullQuote)
+
+        console.log(random)
+        console.log(quotes.length)
+        // quote.innerHTML = quotes[random].text
+        // author.innerHTML = quotes[random].author
       }
-      console.log(random)
-      console.log(quotes.length)
-      quote.innerHTML = quotes[random].text
-      author.innerHTML = quotes[random].author
+      quoteLists.appendChild(quoteList)
     })
     .catch(function (err) {
       console.log(err)
